73e321ef17afff765b4cfc3d35841752840a688a
aulil99 <aulilmushalli@gmail.com>
2024-02-27 11:49:12 +0700

    update views tambah dan edit pengiriman

--- application/views/pengiriman/form_edit.php
+++ application/views/pengiriman/form_edit.php
@@ -104,8 +104,89 @@
         </div>
     </div>
 
+    <!-- Pilih Barang -->
+
+    <div class="row">
+        <div class="col-sm-12 col-md-10">
+            <h4 class="mb-0"><i class="fa fa-cubes"></i> Info Barang</h4>
+        </div>
+        <div class="col-sm-12 col-md-2">
+            <!-- <a href="<?= site_url('tambah_barang'); ?>" class="btn btn-success btn-sm btn-block">Pilih Barang</a> -->
+
+            <button type="button" class="btn btn-success btn-sm btn-block" data-toggle="modal" data-target="#pilihBarang">
+                Pilih Barang
+            </button>
+
+            <!-- Modal -->
+            <div class="modal fade" id="pilihBarang" tabindex="-1" role="dialog" aria-labelledby="pilihBarangLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Pilih Barang</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <table class="table table-sm table-hover table-striped">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col">Kode Barang</th>
+                                    <th scope="col">Nama Barang</th>
+                                    <th scope="col">Brand</th>
+                                    <th scope="col">Stok</th>
+                                    <th scope="col">Harga Jual</th>
+                                    <th scope="col">Opsi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <!-- <button type="button" class="btn btn-success">Save changes</button> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr class="mt-0" />
+    <?php
+    //tampilkan pesan success
+    if ($this->session->flashdata('success')) {
+        echo '<div class="alert alert-success" role="alert">
+    ' . $this->session->flashdata('success') . '
+  </div>';
+    }
+
+    //tampilkan pesan error
+    if ($this->session->flashdata('error')) {
+        echo '<div class="alert alert-danger" role="alert">
+    ' . $this->session->flashdata('error') . '
+  </div>';
+    }
+    ?>
+    <div class="table-responsive">
+        <table class="table table-sm table-hover table-striped" id="tables">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Kode Barang</th>
+                    <th scope="col">Nama Barang</th>
+                    <th scope="col">Brand</th>
+                    <th scope="col">Stok</th>
+                    <th scope="col">Harga Jual</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Opsi</th>
+                </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
+    </div>
+
     <div class="form-group row">
-        <div class="col-sm-9 offset-md-3">
+        <div class="col-sm-9">
             <button type="submit" name="submit" value="submit" class="btn btn-primary btn-sm">Edit Data</button>
             <button type="button" class="btn btn-light btn-sm" onclick="window.history.back()">Kembali</button>
         </div>
